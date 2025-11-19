fd26adb24fef30cd61cffd45c07a3b24fc129dc8
Dotan Dimet <dotan@corky.net>
2025-11-19 11:30:00 +0200

    incorporate bin/macos.sh into install.sh; move stuff into functions

--- install.sh
+++ install.sh
@@ -7,12 +7,65 @@ CONF_DIR=${1:-"${SCRIPT_DIR}/config"} # configs are in ./config, can be overwrit
 TARGET_DIR=${XDG_CONFIG_HOME:-"${HOME}/.config"}
 [[ -d "${TARGET_DIR}" ]] || mkdir -p "${TARGET_DIR}"
 
+# install brew, brew software listed in the Brewfile,
+# and set bash as the default shell
+function install_macos_stuff() {
+  # brew and software installed with brew
+  if [[ ! -x /opt/homebrew/bin/brew ]]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  brew bundle install --file=Brewfile
+
+  # Set Homebrew's bash as default shell
+  BREW_BASH="$(brew --prefix)/bin/bash"
+  grep -Fq "$BREW_BASH" /etc/shells || echo "${BREW_BASH}" | sudo tee -a /etc/shells
+  [[ "$SHELL" == "$BREW_BASH" ]] || chsh -s "${BREW_BASH}"
+}
+
+# Install symlinks for config files
+function symlink_config_files {
+  cd "${CONF_DIR}" || exit
+  for CONF in *; do
+    SRC="${CONF_DIR}/${CONF}"
+    TARGET=""
+    if [[ "${CONF}" == "bashrc" ||
+      "${CONF}" == "bash_profile" ||
+      "${CONF}" == "inputrc" ||
+      "${CONF}" == "vimrc" ||
+      "${CONF}" == "tmux.conf" ]] \
+      ; then
+      TARGET="${HOME}/.${CONF}"
+    else
+      TARGET="${TARGET_DIR}/${CONF}"
+    fi
+    if [[ -L "${TARGET}" ]]; then
+      if [[ "$(readlink "${TARGET}")" != "${SRC}" ]]; then
+        echo "${TARGET} is a symlink but not to ${SRC}, moving to ${TARGET}_bak"
+        mv "${TARGET}" "${TARGET}_bak"
+      fi
+    elif [[ -r "${TARGET}" ]]; then
+      echo "${TARGET} exists but not a link, moving to ${TARGET}_bak"
+      mv "${TARGET}" "${TARGET}_bak"
+    fi
+    # so now, $TARGET shouldn't exist unless it's Cool:
+    if [[ -n "${TARGET}" && ! -e "${TARGET}" ]]; then
+      echo "Installing ${CONF} configuration"
+      ln -s "${SRC}" "${TARGET}" && echo "Installed link in ${TARGET}"
+    fi
+  done
+}
+
+# Install config files
+echo "Installing dotfiles from ${CONF_DIR}"
+symlink_config_files
+
 # Install software:
 echo "Installing software"
 
 if uname -a | grep -q Darwin; then
   echo "Installing macos stuff"
-  "${SCRIPT_DIR}/bin/macos.sh"
+  install_macos_stuff
 fi
 
 if [[ -x ~/.local/bin/mise ]]; then
@@ -20,38 +73,6 @@ if [[ -x ~/.local/bin/mise ]]; then
 else
   curl https://mise.run | sh
 fi
-
-echo "Installing dotfiles from ${CONF_DIR}"
-
-cd "${CONF_DIR}" || exit
-for CONF in *; do
-  SRC="${CONF_DIR}/${CONF}"
-  TARGET=""
-  if [[ "${CONF}" == "bashrc" ||
-    "${CONF}" == "bash_profile" ||
-    "${CONF}" == "inputrc" ||
-    "${CONF}" == "vimrc" ||
-    "${CONF}" == "tmux.conf" ]] \
-    ; then
-    TARGET="${HOME}/.${CONF}"
-  else
-    TARGET="${TARGET_DIR}/${CONF}"
-  fi
-  if [[ -L "${TARGET}" ]]; then
-    if [[ "$(readlink "${TARGET}")" != "${SRC}" ]]; then
-      echo "${TARGET} is a symlink but not to ${SRC}, moving to ${TARGET}_bak"
-      mv "${TARGET}" "${TARGET}_bak"
-    fi
-  elif [[ -r "${TARGET}" ]]; then
-    echo "${TARGET} exists but not a link, moving to ${TARGET}_bak"
-    mv "${TARGET}" "${TARGET}_bak"
-  fi
-  # so now, $TARGET shouldn't exist unless it's Cool:
-  if [[ -n "${TARGET}" && ! -e "${TARGET}" ]]; then
-    echo "Installing ${CONF} configuration"
-    ln -s "${SRC}" "${TARGET}" && echo "Installed link in ${TARGET}"
-  fi
-done
 
 echo "Installing mise software..."
 eval "$(~/.local/bin/mise activate bash --shims)" # so any shims added are available in the installlation
